a231d0d132f304a876dba981b36a0c44ca8bf467
dhruv rathod <62165682+dhruvrathod23@users.noreply.github.com>
2020-12-27 16:43:18 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import * as ReactBootStrap from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Navbar= () => {
+  
+  return (
+    <div className="Navbar">
+    <ReactBootStrap.Navbar bg="light" expand="lg">
+  <ReactBootStrap.Navbar.Brand href="#home">DHRUV RATHOD</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+    <ReactBootStrap.Nav className="mr-auto">
+      <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#link">Link</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Divider />
+        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+      </ReactBootStrap.NavDropdown>
+    </ReactBootStrap.Nav>
+  
+  </ReactBootStrap.Navbar.Collapse>
+</ReactBootStrap.Navbar>
+       </div>
+  );
+};
+
+export default Navbar ;
